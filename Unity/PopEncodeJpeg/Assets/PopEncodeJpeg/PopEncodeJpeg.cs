0328962b76b627a17137168a5f3feaceaea39899
Graham Reeves <graham@grahamreeves.com>
2017-04-16 23:36:57 +0100

    Added test unity project & c# interface

---
+++ Unity/PopEncodeJpeg/Assets/PopEncodeJpeg/PopEncodeJpeg.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;					// required for Coroutines
+using System.Runtime.InteropServices;		// required for DllImport
+using System;								// requred for IntPtr
+using System.Text;
+using System.Collections.Generic;
+
+
+
+/// <summary>
+///	Low level interface
+/// </summary>
+public static class PopEncodeJpeg {
+
+	#if UNITY_IOS && !UNITY_EDITOR_OSX && !UNITY_EDITOR_WIN
+	private const string PluginName = "__Internal";
+	#else
+	private const string PluginName = "PopEncodeJpeg";
+	#endif
+
+	[DllImport (PluginName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern System.IntPtr	PopDebugString();
+
+	[DllImport (PluginName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern void		ReleaseDebugString(System.IntPtr String);
+
+	[DllImport(PluginName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern int		EncodeJpeg (byte[] JpegData, int JpegDataSize, int JpegQuality, byte[] ImageData, int ImageDataSize, int ImageWidth, int ImageHeight, int ImageComponents);
+
+
+
+	public static byte[] EncodeToJpeg(Texture2D Image)
+	{
+		var Width = Image.width;
+		var Height = Image.height;
+		var Pixels = Image.GetPixels32 ();
+		var ComponentCount = 4;
+
+		var PixelBytes = new byte[ComponentCount * Width * Height];
+		for (int i = 0;	i < Pixels.Length;	i++) {
+			var Colour = Pixels [i];
+			int p = i * ComponentCount;
+			PixelBytes [p + 0] = Colour.r;
+			PixelBytes [p + 1] = Colour.g;
+			PixelBytes [p + 2] = Colour.b;
+			PixelBytes [p + 3] = Colour.a;
+		}
+
+		//	try and encode, returns number of bytes used. if the number is bigger than allocated, we need a bigger buffer
+		var JpegData = new byte[ComponentCount * Width * Height];
+		int Quality = 1;
+		var BytesWritten = EncodeJpeg (JpegData, JpegData.Length, Quality, PixelBytes, PixelBytes.Length, Width, Height, ComponentCount);
+		if (BytesWritten > JpegData.Length)
+			throw new System.Exception ("Didn't allocate enough bytes for JPEG. " + JpegData.Length + "/" + BytesWritten);
+
+		var ShrunkJpegData = new Byte[BytesWritten];
+		for (int i = 0;	i < ShrunkJpegData.Length;	i++)
+			ShrunkJpegData [i] = JpegData [i];
+
+		return ShrunkJpegData;
+	}
+
+
+}
---
+++ Unity/PopEncodeJpeg/Assets/SaveTextureToJpeg.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+public class SaveTextureToJpeg : MonoBehaviour {
+
+	#if UNITY_EDITOR
+	[MenuItem("Assets/Texture/Save Texture to Jpeg")]
+	static void _SaveTextureToJpeg()
+	{
+		//	get selected textures
+		string[] AssetGuids = Selection.assetGUIDs;
+		for (int i=0; i<AssetGuids.Length; i++) {
+			string Guid = AssetGuids[i];
+			string Path = AssetDatabase.GUIDToAssetPath (Guid);
+			Texture Tex = AssetDatabase.LoadAssetAtPath( Path, typeof(Texture) ) as Texture;
+			if ( !Tex )
+				continue;
+
+			DoSaveTextureToJpeg( Tex, Path, Guid );
+		}
+	}
+	#endif
+
+	#if UNITY_EDITOR
+	static public bool DoSaveTextureToJpeg(Texture Tex,string AssetPath,string AssetGuid)
+	{
+		string Filename = EditorUtility.SaveFilePanel("save " + AssetPath, "", AssetGuid, "Jpeg");
+		if ( Filename.Length == 0 )
+			return false;
+
+		return DoSaveTextureToJpeg( Tex, Filename );
+	}
+#endif
+
+	static public Texture2D GetTexture2D(Texture Tex)
+	{ 
+		return GetTexture2D( Tex, TextureFormat.RGB24 );
+	}
+
+	static public Texture2D GetTexture2D(Texture Tex,TextureFormat Format)
+	{ 
+		if ( Tex is Texture2D )
+			return Tex as Texture2D;
+	
+		//	copy to render texture and read
+		RenderTexture rt = RenderTexture.GetTemporary( Tex.width, Tex.height, 0, RenderTextureFormat.ARGBFloat );
+		Graphics.Blit( Tex, rt );
+		Texture2D Temp = new Texture2D( rt.width, rt.height, Format, false );
+		RenderTexture.active = rt;
+		Temp.ReadPixels( new Rect(0,0,rt.width,rt.height), 0, 0 );
+		Temp.Apply();
+		RenderTexture.active = null;
+		RenderTexture.ReleaseTemporary( rt );
+
+		return Temp;
+	}
+
+	static public bool DoSaveTextureToJpeg(Texture Tex,string Filename)
+	{
+		var Temp = GetTexture2D( Tex );
+
+		byte[] Bytes = PopEncodeJpeg.EncodeToJpeg( Temp );
+		File.WriteAllBytes( Filename, Bytes );
+		return true;
+	}
+}
