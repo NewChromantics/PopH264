2b398570d24d9ae3f7a7c0f1673160c1f7d15858
Graham Reeves <graham@grahamreeves.com>
2017-04-18 19:40:12 +0100

    Added UWP project and some windows includes

--- Source/PopEncodeJpeg.hpp
+++ Source/PopEncodeJpeg.hpp
@@ -1,5 +1,18 @@
 #pragma once
 
+
+#if defined(_MSC_VER)
+#define TARGET_WINDOWS
+#endif
+
+#if defined(TARGET_WINDOWS)
+#include <SDKDDKVer.h>
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#endif
+
+
 #include <cstdint>
 #include "Unity/IUnityInterface.h"
 
