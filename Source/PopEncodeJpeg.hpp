7ae29faf428936714e598630fd96b3e53374adb9
Graham Reeves <graham@grahamreeves.com>
2017-04-21 19:31:39 +0100

    Fixed exports for windows

--- Source/PopEncodeJpeg.hpp
+++ Source/PopEncodeJpeg.hpp
@@ -16,7 +16,7 @@
 #include <cstdint>
 #include "Unity/IUnityInterface.h"
 
-#define __export			extern "C"
+#define __export			extern "C" __declspec(dllexport)
 
 
 __export int32_t			EncodeJpeg(uint8_t* JpegData,int32_t JpegDataSize,int32_t JpegQuality,uint8_t* ImageData,int32_t ImageDataSize,int32_t ImageWidth,int32_t ImageHeight,int32_t ImageComponents);
