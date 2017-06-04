1691cae7769c1053b817276a179dd45bb0323397
Graham Reeves <graham@grahamreeves.com>
2017-06-04 19:48:46 +0100

    OSX compile fix

--- Source/PopEncodeJpeg.hpp
+++ Source/PopEncodeJpeg.hpp
@@ -16,8 +16,11 @@
 #include <cstdint>
 #include "Unity/IUnityInterface.h"
 
+#if defined(TARGET_WINDOWS)
 #define __export			extern "C" __declspec(dllexport)
-
+#else
+#define __export			extern "C"
+#endif
 
 __export int32_t			EncodeJpeg(uint8_t* JpegData,int32_t JpegDataSize,int32_t JpegQuality,uint8_t* ImageData,int32_t ImageDataSize,int32_t ImageWidth,int32_t ImageHeight,int32_t ImageComponents,int32_t IsRgb);
 
