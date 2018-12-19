11aba1ee89005a3d3e086af4d23ae52463b7a9f0
Graham Reeves <graham@grahamreeves.com>
2018-12-19 15:37:46 +0000

    Unity project working with DLL

---
+++ Unity/PopCameraDevice/Assets/DebugCameraDevices.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugCameraDevices : MonoBehaviour {
+
+	void Update ()
+	{
+		var DeviceNames = PopCameraDevice.EnumCameraDevices();
+		DeviceNames.ForEach( Debug.Log );
+	}
+}
---
+++ Unity/PopCameraDevice/Assets/PopCameraDevice/PopCameraDevice.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;					// required for Coroutines
+using System.Runtime.InteropServices;		// required for DllImport
+using System;								// requred for IntPtr
+using System.Text;
+using System.Collections.Generic;
+
+
+
+/// <summary>
+///	Low level interface
+/// </summary>
+public static class PopCameraDevice
+{
+	private const string PluginName = "PopCameraDevice";
+
+	//	use byte as System.Char is a unicode char (2 bytes), then convert to Unicode Char
+	[DllImport(PluginName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern void		EnumCameraDevices([In, Out] byte[] StringBuffer,int StringBufferLength);
+
+
+	public static List<string> EnumCameraDevices()
+	{
+		var StringBuffer = new byte[1000];
+		EnumCameraDevices( StringBuffer, StringBuffer.Length );
+
+		//	split strings
+		var Delin = StringBuffer[0];
+		string CurrentName = "";
+		var Names = new List<string>();
+		System.Action FinishCurrentName = ()=>
+		{
+			if ( String.IsNullOrEmpty(CurrentName) )
+				return;
+			Names.Add(CurrentName);
+			CurrentName = "";
+		};
+
+		//	split at delin or when we hit a terminator
+		for ( var i=1;	i<StringBuffer.Length;	i++ )
+		{
+			var Char8 = StringBuffer[i];
+			if ( Char8 == '\0' )
+			{
+				FinishCurrentName();
+				break;
+			}
+			if ( Char8 == Delin )
+			{
+				FinishCurrentName();
+				continue;
+			}
+			var UnicodeChar = System.Convert.ToChar(Char8);
+			CurrentName += UnicodeChar;
+		}
+		FinishCurrentName();
+
+		return Names;
+	}
+	
+
+}
