25d7eb4d9775e28696207ac6ab8e781b7519f904
Graham Reeves <graham@grahamreeves.com>
2019-01-17 20:19:16 +0000

    Renaming and stripping

--- Source/PopCameraDevice.hpp
+++ Source/PopCameraDevice.hpp
@@ -10,8 +10,6 @@
 #endif
 
 
-#include <cstdint>
-#include "Unity/IUnityInterface.h"
 
 #if defined(TARGET_WINDOWS)
 #define __export			extern "C" __declspec(dllexport)
@@ -19,12 +17,5 @@
 #define __export			extern "C"
 #endif
 
-__export void				EnumCameraDevices(char* StringBuffer,int32_t StringBufferLength);
-__export int32_t			CreateCameraDevice(const char* Name);
-__export void				FreeCameraDevice(int32_t Instance);
-__export void				GetMeta(int32_t Instance,int32_t* MetaValues,int32_t MetaValuesCount);
-__export int32_t			PopFrame(int32_t Instance,uint8_t* Plane0,int32_t Plane0Size,uint8_t* Plane1,int32_t Plane1Size,uint8_t* Plane2,int32_t Plane2Size);
+__export int32_t			GetTestInteger();
 
-
-__export const char*		PopDebugString();
-__export void				ReleaseDebugString(const char* String);
