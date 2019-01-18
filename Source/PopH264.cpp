d7954b1a14ebd91e37b6eac34a582de46302926d
Graham Reeves <graham@grahamreeves.com>
2019-01-18 18:40:26 +0000

    Abstracted instance manager, added intermediate frame storage, inputting & outputting data

--- Source/PopH264.cpp
+++ Source/PopH264.cpp
@@ -1,8 +1,39 @@
 #include "PopH264.h"
-#include <memory>
-#include <mutex>
-#include "SoyLib/src/HeapArray.hpp"
 #include "BroadwayDecoder.h"
+#include "SoyLib/src/SoyPixels.h"
+
+class TNoParams;
+class TDecoderInstance;
+using TInstanceObject = TDecoderInstance;
+using TInstanceParams = TNoParams;
+
+#include "InstanceManager.inc"
+
+class TFrame
+{
+public:
+	std::shared_ptr<SoyPixelsImpl>	mPixels;
+	uint32_t						mTimeMs;
+};
+	
+class TDecoderInstance
+{
+public:
+	void									PushData(const uint8_t* Data,size_t DataSize);
+	void									PopFrame(int32_t& FrameTimeMs,ArrayBridge<uint8_t>&& Plane0,ArrayBridge<uint8_t>&& Plane1,ArrayBridge<uint8_t>&& Plane2);
+	bool									PopFrame(TFrame& Frame);
+	void									PushFrame(const SoyPixelsImpl& Frame);
+	const SoyPixelsMeta&					GetMeta() const	{	return mMeta;	}
+	
+private:
+	uint32_t								mFrameCounter = 0;
+	std::shared_ptr<Broadway::TDecoder>		mDecoder;
+	std::mutex								mFramesLock;
+	Array<TFrame>							mFrames;
+	SoyPixelsMeta							mMeta;
+};
+
+
 
 #if defined(TARGET_WINDOWS)
 BOOL APIENTRY DllMain(HMODULE /* hModule */, DWORD ul_reason_for_call, LPVOID /* lpReserved */)
@@ -20,165 +51,77 @@ BOOL APIENTRY DllMain(HMODULE /* hModule */, DWORD ul_reason_for_call, LPVOID /*
 #endif
 
 
-__export int32_t GetTestInteger()
+
+void TDecoderInstance::PushData(const uint8_t* Data,size_t DataSize)
 {
-	return 12345;
-}
-
-
-class TNoParams
-{
-	
-};
-
-
-using TInstanceObject = Broadway::TDecoder;
-class TInstance;
-using TInstanceParams = TNoParams;
-
-namespace InstanceManager
-{
-	TInstanceObject&	GetInstance(uint32_t Instance);
-	uint32_t			AssignInstance(std::shared_ptr<TInstanceObject> Object);
-	void				FreeInstance(uint32_t Instance);
-	uint32_t			CreateInstance(const TInstanceParams& Params);
-	
-	std::mutex			InstancesLock;
-	Array<TInstance>	Instances;
-	uint32_t			InstancesCounter = 1;
-}
-
-
-class TInstance
-{
-public:
-	std::shared_ptr<TInstanceObject>	mObject;
-	uint32_t							mInstanceId = 0;
-	
-	bool								operator==(const uint32_t& InstanceId) const	{	return mInstanceId == InstanceId;	}
-};
-
-
-
-uint32_t InstanceManager::CreateInstance(const TInstanceParams& Params)
-{
-	//	alloc device
-	try
+	auto DataArray = GetRemoteArray( Data, DataSize );
+	auto PushFrame = [this](const SoyPixelsImpl& Pixels)
 	{
-		auto Object = std::make_shared<Broadway::TDecoder>();
-		if ( Object )
-			return InstanceManager::AssignInstance(Object);
-	}
-	catch(std::exception& e)
-	{
-		std::Debug << e.what() << std::endl;
-	}
-	
-	
-	throw Soy::AssertException("Failed to create instance");
+		this->PushFrame( Pixels );
+	};
+	mDecoder->Decode( GetArrayBridge(DataArray), PushFrame );
 }
 
-
-
-
-TInstanceObject& InstanceManager::GetInstance(uint32_t Instance)
+void TDecoderInstance::PopFrame(int32_t& FrameTimeMs,ArrayBridge<uint8_t>&& Plane0,ArrayBridge<uint8_t>&& Plane1,ArrayBridge<uint8_t>&& Plane2)
 {
-	std::lock_guard<std::mutex> Lock(InstancesLock);
-	auto pInstance = Instances.Find(Instance);
-	auto* Device = pInstance ? pInstance->mObject.get() : nullptr;
-	if ( !Device )
+	TFrame Frame;
+	if ( !PopFrame( Frame ) )
 	{
-		std::stringstream Error;
-		Error << "No instance/device matching " << Instance;
-		throw Soy::AssertException(Error.str());
-	}
-	
-	return *Device;
-}
-
-
-uint32_t InstanceManager::AssignInstance(std::shared_ptr<TInstanceObject> Object)
-{
-	std::lock_guard<std::mutex> Lock(InstancesLock);
-	
-	TInstance Instance;
-	Instance.mInstanceId = InstancesCounter;
-	Instance.mObject = Object;
-	Instances.PushBack(Instance);
-	
-	InstancesCounter++;
-	return Instance.mInstanceId;
-}
-
-
-void InstanceManager::FreeInstance(uint32_t Instance)
-{
-	std::lock_guard<std::mutex> Lock(InstancesLock);
-	
-	auto InstanceIndex = Instances.FindIndex(Instance);
-	if ( InstanceIndex < 0 )
-	{
-		std::Debug << "No instance " << Instance << " to free" << std::endl;
+		FrameTimeMs = -1;
 		return;
 	}
 	
-	Instances.RemoveBlock(InstanceIndex, 1);
-}
+	//	emulating TPixelBuffer interface
+	BufferArray<SoyPixelsImpl*, 10> Textures;
+	Textures.PushBack( Frame.mPixels.get() );
 
-
-
-template<typename RETURN,typename FUNC>
-RETURN SafeCall(FUNC Function,const char* FunctionName,RETURN ErrorReturn)
-{
-	try
+	BufferArray<std::shared_ptr<SoyPixelsImpl>, 10> Planes;
+	
+	//	get all the planes
+	for ( auto t = 0; t < Textures.GetSize(); t++ )
 	{
-		return Function();
+		auto& Texture = *Textures[t];
+		Texture.SplitPlanes(GetArrayBridge(Planes));
 	}
-	catch(std::exception& e)
+	
+	ArrayBridge<uint8_t>* PlanePixels[] = { &Plane0, &Plane1, &Plane2 };
+	for ( auto p = 0; p < Planes.GetSize() && p<3; p++ )
 	{
-		std::Debug << FunctionName << " exception: " << e.what() << std::endl;
-		return ErrorReturn;
+		auto& Plane = *Planes[p];
+		auto& PlaneDstPixels = *PlanePixels[p];
+		auto& PlaneSrcPixels = Plane.GetPixelsArray();
+		
+		auto MaxSize = std::min(PlaneDstPixels.GetDataSize(), PlaneSrcPixels.GetDataSize());
+		//	copy as much as possible
+		auto PlaneSrcPixelsMin = GetRemoteArray(PlaneSrcPixels.GetArray(), MaxSize);
+		PlaneDstPixels.Copy(PlaneSrcPixelsMin);
 	}
-	catch(...)
-	{
-		std::Debug << FunctionName << " unknown exception." << std::endl;
-		return ErrorReturn;
-	}
+
 }
 
-
-
-__export int32_t CreateInstance()
+bool TDecoderInstance::PopFrame(TFrame& Frame)
 {
-	auto Function = [&]()
-	{
-		auto InstanceId = InstanceManager::CreateInstance( TInstanceParams() );
-		return InstanceId;
-	};
-	return SafeCall( Function, __func__, -1 );
+	std::lock_guard<std::mutex> Lock(mFramesLock);
+	if ( mFrames.IsEmpty() )
+		return false;
+	
+	Frame = mFrames[0];
+	mFrames.RemoveBlock(0,1);
+	return true;
 }
 
-__export void DestroyInstance(int32_t Instance)
+void TDecoderInstance::PushFrame(const SoyPixelsImpl& Frame)
 {
-	auto Function = [&]()
-	{
-		InstanceManager::FreeInstance(Instance);
-		return 0;
-	};
-	SafeCall(Function, __func__, 0 );
+	TFrame NewFrame;
+	NewFrame.mTimeMs = mFrameCounter++;
+	NewFrame.mPixels.reset( new SoyPixels( Frame ) );
+
+	std::lock_guard<std::mutex> Lock(mFramesLock);
+	mFrames.PushBack(NewFrame);
+	mMeta = Frame.GetMeta();
 }
 
-
-namespace Decoder
-{
-	void	PopFrame(Broadway::TDecoder& Decoder,int32_t& FrameTimeMs,ArrayBridge<uint8_t>&& Plane0,ArrayBridge<uint8_t>&& Plane1,ArrayBridge<uint8_t>&& Plane2);
-}
-
-void Decoder::PopFrame(Broadway::TDecoder& Decoder,int32_t& FrameTimeMs,ArrayBridge<uint8_t>&& Plane0,ArrayBridge<uint8_t>&& Plane1,ArrayBridge<uint8_t>&& Plane2)
-{
-	//Decoder.PopFrame(Plane0, Plane1, Plane2, FrameTimeMs );
-	throw Soy::AssertException("Todo; Decoder.PopFrame");
-}
+	
 
 __export int32_t PopFrame(int32_t Instance,uint8_t* Plane0,int32_t Plane0Size,uint8_t* Plane1,int32_t Plane1Size,uint8_t* Plane2,int32_t Plane2Size)
 {
@@ -190,7 +133,7 @@ __export int32_t PopFrame(int32_t Instance,uint8_t* Plane0,int32_t Plane0Size,ui
 		auto Plane1Array = GetRemoteArray(Plane1, Plane1Size);
 		auto Plane2Array = GetRemoteArray(Plane2, Plane2Size);
 		int32_t FrameTimeMs = -1;
-		Decoder::PopFrame( Decoder, FrameTimeMs, GetArrayBridge(Plane0Array), GetArrayBridge(Plane1Array), GetArrayBridge(Plane2Array));
+		Decoder.PopFrame( FrameTimeMs, GetArrayBridge(Plane0Array), GetArrayBridge(Plane1Array), GetArrayBridge(Plane2Array));
 		return FrameTimeMs;
 	};
 	return SafeCall(Function, __func__, -99 );
@@ -201,9 +144,10 @@ __export int32_t PushData(int32_t Instance,uint8_t* Data,int32_t DataSize)
 	auto Function = [&]()
 	{
 		auto& Decoder = InstanceManager::GetInstance(Instance);
+		Decoder.PushData( Data, DataSize );
 		return 0;
 	};
-	return SafeCall(Function, __func__, 0 );
+	return SafeCall(Function, __func__, -1 );
 }
 
 
@@ -212,7 +156,7 @@ __export void GetMeta(int32_t Instance, int32_t* pMetaValues, int32_t MetaValues
 	auto Function = [&]()
 	{
 		auto& Device = InstanceManager::GetInstance(Instance);
-		/*
+		
 		auto& Meta = Device.GetMeta();
 		
 		size_t MetaValuesCounter = 0;
@@ -231,7 +175,7 @@ __export void GetMeta(int32_t Instance, int32_t* pMetaValues, int32_t MetaValues
 			MetaValues.PushBack(PlaneMeta.GetFormat());
 			MetaValues.PushBack(PlaneMeta.GetDataSize());
 		}
-		*/
+		
 		return 0;
 	};
 	auto x = SafeCall(Function, __func__, 0 );
