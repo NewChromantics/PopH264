b4984e6fa17aa4c8c90e2b0fe003480160dc2b49
Graham Reeves <graham@grahamreeves.com>
2019-11-22 16:07:38 +0000

    Changed lumin build to a script so I can copy to unity dir

---
+++ PopH264.Lumin/build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+if [ -z "$UNITY_ASSET_PLUGIN_PATH" ]; then
+	echo "UNITY_ASSET_PLUGIN_PATH env var not set"
+	exit 1
+fi
+
+echo "SOURCE_ROOT = $SOURCE_ROOT"
+$MAGIC_LEAP_SDK_PATH/mabu -b $MABU_FILENAME -t $MABU_BUILD_TARGET --out $SOURCE_ROOT/PopH264.Lumin/$MABU_BUILD_TARGET/
+
+RESULT=$?
+
+if [[ $RESULT -ne 0 ]]; then
+	exit $RESULT
+fi
+
+SRC_PATH="PopH264.Lumin/release_ml1/release_lumin_clang-3.8_aarch64/libPopH264.so"
+DEST_PATH="$UNITY_ASSET_PLUGIN_PATH"
+echo "Copying $SRC_PATH to $DEST_PATH"
+
+mkdir -p $DEST_PATH && cp $SRC_PATH $DEST_PATH
+
+RESULT=$?
+if [[ $RESULT -ne 0 ]]; then
+	exit $RESULT
+fi
+
+exit 0
