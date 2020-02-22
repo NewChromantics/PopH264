d743ff29141eeb5232db38998f1a209cb980ffec
Graham Reeves <graham@grahamreeves.com>
2020-02-22 23:29:48 +0000

    check for Magic leap sdk env var

--- PopH264.Lumin/build.sh
+++ PopH264.Lumin/build.sh
@@ -5,6 +5,12 @@ if [ -z "$UNITY_ASSET_PLUGIN_PATH" ]; then
 	exit 1
 fi
 
+# new magic leap lab now sets MLSDK
+if [ -z "$MAGIC_LEAP_SDK_PATH" ]; then
+	echo "MAGIC_LEAP_SDK_PATH (magic leap sdk, eg /Volumes/Assets/mlsdk/v0.23.0) env var not set"
+	exit 1
+fi
+
 echo "SOURCE_ROOT = $SOURCE_ROOT"
 $MAGIC_LEAP_SDK_PATH/mabu -b $MABU_FILENAME -t $MABU_BUILD_TARGET --out $SOURCE_ROOT/PopH264.Lumin/$MABU_BUILD_TARGET/
 
