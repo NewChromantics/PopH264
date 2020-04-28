a8cde6bed35c6bb9b1b6f7ca4d82af828e16c0cf
Graham Reeves <graham@grahamreeves.com>
2020-04-28 10:31:23 +0100

    Updated version to 1.2.7

--- Source/PopH264.cpp
+++ Source/PopH264.cpp
@@ -15,7 +15,8 @@ namespace PopH264
 	//	1.2.4	Added ProfileLevel
 	//	1.2.5	Encoder now uses .Keyframe meta setting
 	//	1.2.6	X264 now uses ProfileLevel + lots of x264 settings exposed
-	const Soy::TVersion	Version(1,2,6);
+	//	1.2.7	Added MediaFoundation decoder to windows
+	const Soy::TVersion	Version(1,2,7);
 }
 
 
