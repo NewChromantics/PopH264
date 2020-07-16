ee16f85d4bf1007dbb29222f80eff226c2504b86
thomasSDK <tsdkelly@protonmail.com>
2020-07-16 13:17:38 +0100

    async

--- .github/actions/ios/index.js
+++ .github/actions/ios/index.js
@@ -1,12 +1,15 @@
-const core = require('@actions/core');
+const core = require("@actions/core");
 const exec = require("@actions/exec");
 
-try {
-  process.env.BUILDPROJECT = core.getInput("BuildProject");
-  process.env.BUILDSCHEME = core.getInput("BuildScheme");
-  
-  await exec.exec(`/bin/bash ${__dirname}/build.sh`);
+async function run() {
+  try {
+    process.env.BUILDPROJECT = core.getInput("BuildProject");
+    process.env.BUILDSCHEME = core.getInput("BuildScheme");
 
-} catch (error) {
-  core.setFailed(error.message);
+    await exec.exec(`/bin/bash ${__dirname}/build.sh`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run();
