b401d7cfd91efe99c4f82dd053106efb072940e0
thomasSDK <tsdkelly@protonmail.com>
2020-07-16 14:19:30 +0100

    set defaults

--- .github/actions/linux/build.sh
+++ .github/actions/linux/build.sh
@@ -3,7 +3,8 @@
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 sudo apt-get install libx264-dev gcc-10 g++-10 -y
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 1
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 10
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-10 10
 
 g++ -v
 
