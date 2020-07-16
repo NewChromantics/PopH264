f826b144a8148b7490ff2a12769390d6e7401116
thomasSDK <tsdkelly@protonmail.com>
2020-07-16 16:24:23 +0100

    ios build dir

--- .github/actions/ios/build.sh
+++ .github/actions/ios/build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 
-xcodebuild -workspace $BUILDPROJECT/project.xcworkspace -scheme $BUILDSCHEME -showBuildSettings | grep TARGET_BUILD_DIR | sed -e 's/.*TARGET_BUILD_DIR = //'
+$builddir = xcodebuild -workspace $BUILDPROJECT/project.xcworkspace -scheme $BUILDSCHEME -showBuildSettings | grep TARGET_BUILD_DIR | sed -e 's/.*TARGET_BUILD_DIR = //'
 
 xcodebuild -workspace $BUILDPROJECT/project.xcworkspace -scheme $BUILDSCHEME -showBuildSettings
 xcodebuild -workspace $BUILDPROJECT/project.xcworkspace -list
 xcodebuild -workspace $BUILDPROJECT/project.xcworkspace -scheme $BUILDSCHEME
 
-echo "Build Directory ${BUILDPROJECT} contents"
+echo "Build Directory for ${BUILDSCHEME} contents"
 
-ls
+ls $builddir
 
-mkdir -p ./build/${BUILDSCHEME}
-mv $BUILDPROJECT ./build/${BUILDSCHEME}
+# mkdir -p ./build/${BUILDSCHEME}
+# mv $BUILDPROJECT ./build/${BUILDSCHEME}
