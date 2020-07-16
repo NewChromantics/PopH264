11a414036b6930c70b66689950932b4e1f3ff202
thomasSDK <tsdkelly@protonmail.com>
2020-07-16 18:55:43 +0100

    add options to parse output

--- .github/actions/ios/index.js
+++ .github/actions/ios/index.js
@@ -7,17 +7,37 @@ const artifactClient = artifact.create();
 const BuildScheme = core.getInput("BuildScheme");
 const BuildProject = core.getInput("BuildProject");
 
+const regex = /TARGET_BUILD_DIR = [^\n]+\n/;
+
 async function run() {
   try {
-    let regex = /TARGET_BUILD_DIR = [^\n]+\n/;
+    const options = {};
+    options.listeners = {
+      stdout: (data) => {
+        myOutput += data.toString();
+        myOutput = regex.exec(myOutput)
+      },
+      stderr: (data) => {
+        myError += data.toString();
+      },
+    };
+
     const buildsettings = await exec.exec("xcodebuild", [
-      `-workspace`, `${BuildProject}/project.xcworkspace`, `-scheme`, `${BuildScheme}`, `-showBuildSettings`,
-    ]);
+      `-workspace`,
+      `${BuildProject}/project.xcworkspace`,
+      `-scheme`,
+      `${BuildScheme}`,
+      `-showBuildSettings`,
+    ], options);
+    
     console.log(buildsettings);
     const buildDirectory = regex.exec(buildsettings);
     console.log(buildDirectory);
     await exec.exec("xcodebuild", [
-      `-workspace`, `${BuildProject}/project.xcworkspace`, `-scheme ${BuildScheme}`,
+      `-workspace`,
+      `${BuildProject}/project.xcworkspace`,
+      `-scheme`,
+      `${BuildScheme}`,
     ]);
 
     const files = ["PopH264_Ios.framework", "PopH264_Ios.framework.dSYM"];
