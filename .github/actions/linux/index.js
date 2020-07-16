a46956d1175449b4da44e1a880c7d76fc4903c03
thomasSDK <tsdkelly@protonmail.com>
2020-07-16 14:45:28 +0100

    correct parameters passed to sh

--- .github/actions/linux/index.js
+++ .github/actions/linux/index.js
@@ -3,10 +3,8 @@ const exec = require("@actions/exec");
 
 async function run() {
   try {
-    process.env.BUILDSOLUTION = core.getInput("BuildSolution");
-    process.env.BUILDPLATFORM = core.getInput("BuildPlatform");
-    process.env.BUILDCONFIGURATION = core.getInput("BuildConfiguration");
-    process.env.BUILDDIRECTORY = core.getInput("BuildDirectory");
+    process.env.ARCHITECTURE = core.getInput("architecture");
+    process.env.MAKEFILE = core.getInput("makefile");
 
     await exec.exec(`/bin/bash ${__dirname}/build.sh`);
   } catch (error) {
