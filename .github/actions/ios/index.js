28933c1a2f0519e772b6a88f03eb149704efc68c
thomasSDK <tsdkelly@protonmail.com>
2020-07-16 18:44:20 +0100

    separating for ios

--- .github/actions/ios/index.js
+++ .github/actions/ios/index.js
@@ -11,13 +11,13 @@ async function run() {
   try {
     let regex = /TARGET_BUILD_DIR = [^\n]+\n/;
     const buildsettings = await exec.exec("xcodebuild", [
-      `-workspace ${BuildProject}/project.xcworkspace`, `-scheme ${BuildScheme} -showBuildSetting`,
+      `-workspace`, `${BuildProject}/project.xcworkspace`, `-scheme`, `${BuildScheme}`, `-showBuildSetting`,
     ]);
     console.log(buildsettings);
     const buildDirectory = regex.exec(buildsettings);
     console.log(buildDirectory);
     await exec.exec("xcodebuild", [
-      `-workspace ${BuildProject}/project.xcworkspace`, `-scheme ${BuildScheme}`,
+      `-workspace`, `${BuildProject}/project.xcworkspace`, `-scheme ${BuildScheme}`,
     ]);
 
     const files = ["PopH264_Ios.framework", "PopH264_Ios.framework.dSYM"];
