ec70036b142ccddbe687c36fae0baeaf26fc6f29
thomasSDK <tsdkelly@protonmail.com>
2020-07-17 09:27:35 +0100

    windows js setup

--- .github/actions/windows/index.js
+++ .github/actions/windows/index.js
@@ -3,12 +3,18 @@ const exec = require("@actions/exec");
 
 async function run() {
   try {
-    process.env.BUILDSOLUTION = core.getInput("BuildSolution");
-    process.env.BUILDPLATFORM = core.getInput("BuildPlatform");
-    process.env.BUILDCONFIGURATION = core.getInput("BuildConfiguration");
-    process.env.BUILDDIRECTORY = core.getInput("BuildDirectory");
+    const BUILDSOLUTION = core.getInput("BuildSolution");
+    const BUILDPLATFORM = core.getInput("BuildPlatform");
+    const BUILDCONFIGURATION = core.getInput("BuildConfiguration");
+    const BUILDDIRECTORY = core.getInput("BuildDirectory");
 
     await exec.exec(`powershell.exe ${__dirname}/build.ps1`);
+
+    await exec.exec('Write-Host', [BUILDSOLUTION])
+    await exec.exec('set')
+    await exec.exec('MSBuild',[BUILDSOLUTION, `property:Configuration=${BUILDCONFIGURATION}`, `property:Platform=${BUILDPLATFORM}`])
+    await exec.exec('ls', [BUILDDIRECTORY])
+
   } catch (error) {
     core.setFailed(error.message);
   }
