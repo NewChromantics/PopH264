b7d482386891e49a6a0857f74249fb2c3f0401a4
thomasSDK <tsdkelly@protonmail.com>
2020-07-17 13:08:51 +0100

    test package with linux
    Cleaned up linux build folder

--- .github/actions/linux/index.js
+++ .github/actions/linux/index.js
@@ -3,7 +3,6 @@ const github = require("@actions/github");
 const exec = require("@actions/exec");
 const artifact = require("@actions/artifact");
 
-const makefile = core.getInput("makefile");
 const architecture = core.getInput("architecture");
 
 const artifactClient = artifact.create();
@@ -11,34 +10,50 @@ const artifactName = `linux-${architecture}`;
 
 async function run() {
   try {
-
-    console.log( await exec.exec("ls") )
+    console.log(await exec.exec("ls"));
     process.env.ARCHITECTURE = architecture;
-    if(makefile === 'Makefile') {
-    await exec.exec("sudo", [
-      `add-apt-repository`, `-y`, `ppa:ubuntu-toolchain-r/test`,
-    ]);
-    await exec.exec("sudo", [`apt-get`, `update`]);
-    await exec.exec("sudo", [
-      `apt-get`, `install`, `libx264-dev`, `gcc-10`, `g++-10`, `-y`,
-    ]);
-    await exec.exec("sudo", [
-      `update-alternatives`, `--install`, `/usr/bin/gcc`, `gcc`, `/usr/bin/gcc-10`, `10`,
-    ]);
-    await exec.exec("sudo", [
-      `update-alternatives`, `--install`, `/usr/bin/g++`, `g++`, `/usr/bin/g++-10`, `10`,
-    ]);
-  }
 
-    await exec.exec("make", [
-      `-f`, `${makefile}`, `exec`, `-C`, `PopH264.Linux/`,
-    ]);
+    // For Gihub hosted runners need to update gcc and get libx264
+    if (architecture === "x86_64") {
+      await exec.exec("sudo", [
+        `add-apt-repository`,
+        `-y`,
+        `ppa:ubuntu-toolchain-r/test`,
+      ]);
+      await exec.exec("sudo", [`apt-get`, `update`]);
+      await exec.exec("sudo", [
+        `apt-get`,
+        `install`,
+        `libx264-dev`,
+        `gcc-10`,
+        `g++-10`,
+        `-y`,
+      ]);
+      await exec.exec("sudo", [
+        `update-alternatives`,
+        `--install`,
+        `/usr/bin/gcc`,
+        `gcc`,
+        `/usr/bin/gcc-10`,
+        `10`,
+      ]);
+      await exec.exec("sudo", [
+        `update-alternatives`,
+        `--install`,
+        `/usr/bin/g++`,
+        `g++`,
+        `/usr/bin/g++-10`,
+        `10`,
+      ]);
+    }
+
+    await exec.exec("make", [`exec`, `-C`, `PopH264.Linux/`]);
 
     const files = [
       `PopH264.Linux/PopH264-${architecture}.so`,
       `PopH264.Linux/PopH264TestApp-${architecture}`,
     ];
-    
+
     const rootDirectory = ".";
 
     const options = {
