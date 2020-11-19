229ba3e67f5ca0ce52322fb15cdaf4844a148f95
Graham Reeves <graham@grahamreeves.com>
2020-11-19 23:18:52 +0000

    Adding ZipFileReader class & java for reading files from apk

--- Unity/PopH264/Assets/PacketParsers/FileReader.cs
+++ Unity/PopH264/Assets/PacketParsers/FileReader.cs
@@ -1,5 +1,7 @@
 //#if NET_4_6	//	gr: not working?
-#if true
+#if UNITY_ANDROID
+#define USE_JAVA_FILEHANDLE
+#else
 //#define USE_MEMORY_MAPPED_FILE
 #define USE_FILE_HANDLE
 #endif
@@ -35,6 +37,89 @@ public abstract class FileReaderBase : MonoBehaviour
 	}
 }
 
+public class JavaFileReader
+{
+	public JavaFileReader(string InternalFilename, string JarOrZipOrApkFilename=null)
+	{
+		//	default to app's apk
+		//	 "jar: file://" + Application.dataPath + "!/assets/" + Filename;
+		if (JarOrZipOrApkFilename == null || JarOrZipOrApkFilename.Length==0)
+			JarOrZipOrApkFilename = Application.dataPath;
+
+		//InternalFilename = "!/assets/" + InternalFilename;
+		InternalFilename = "assets/" + InternalFilename;
+
+		AndroidJavaObject ZipFile;
+		try
+		{
+			//	"com.android.vending.expansion.zipfile.ZipResourceFile"
+			//	"com.google.android.vending.expansion.zipfile"
+			var PackageName = "com.google.android.vending.expansion.zipfile";   //	package name inside the .java
+			var ClassName = "ZipResourceFile";  //	class inside .java
+			ZipFile = new AndroidJavaObject(PackageName + "." + ClassName, JarOrZipOrApkFilename);
+		}
+		catch (AndroidJavaException e)
+		{
+			throw new System.Exception("Failed to open zip file (" + JarOrZipOrApkFilename + "): " + e.Message);
+		}
+		/*
+		var StringSig = "Ljava/lang/String;";
+		var VoidSig = "V";
+
+		var ZipClass = AndroidJNI.FindClass("com.android.vending.expansion.zipfile.ZipResourceFile");
+		var ConstructorName = "<init>";
+		var ConstructorSignature = "("+StringSig+")" + VoidSig +"";    //	1 string arg, return void
+		var ConstructorMethod = AndroidJNI.GetMethodID(ZipClass, ConstructorName, ConstructorSignature);
+		var JarOrZipOrApkFilename_j = AndroidJNI.NewString(JarOrZipOrApkFilename);
+
+		var ConstructorArguments = new jvalue[1];
+		ConstructorArguments[0].l = JarOrZipOrApkFilename_j;	//	.l = L in signature
+		ZipFile = AndroidJNI.NewObject(ZipClass, ConstructorMethod, ConstructorArguments);
+		if (ZipFile==System.IntPtr.Zero)
+		*/
+		if ( ZipFile == null)
+			throw new System.Exception("Failed to open zip/jar/apk " + JarOrZipOrApkFilename);
+
+		var GetAssetFileDescriptorName = "getAssetFileDescriptor";
+		AndroidJavaObject FileDescriptor;
+		try
+		{
+			FileDescriptor = ZipFile.Call<AndroidJavaObject>(GetAssetFileDescriptorName, InternalFilename);
+			/*
+			var AssetFileDescriptorClassName = "android/content/res/AssetFileDescriptor";
+			var AssetFileDescriptorSig = "L" + AssetFileDescriptorClassName + ";";
+			var getAssetFileDescriptorSignature = "(" + StringSig + ")" + AssetFileDescriptorSig + "";
+			var getAssetFileDescriptorMethod = AndroidJNI.GetMethodID(ZipClass, GetAssetFileDescriptorName, getAssetFileDescriptorSignature);
+			var InternalFilename_j = AndroidJNI.NewString(InternalFilename);
+			var GetAssetFileDescriptorArguments = new jvalue[1];
+			GetAssetFileDescriptorArguments[0].l = InternalFilename_j;
+			var FileDescriptor = AndroidJNI.CallObjectMethod(ZipFile, getAssetFileDescriptorMethod, GetAssetFileDescriptorArguments);
+			//	gr: returns a null object when filenot found (no exception set in java vm!)
+			if (FileDescriptor == System.IntPtr.Zero)
+			*/
+			if (FileDescriptor == null)
+				throw new System.Exception("Opened zip but failed to open " + InternalFilename);
+		}
+		catch (AndroidJavaException e)
+		{
+			throw new System.Exception("Failed to open file inside zip file (" + InternalFilename + "): " + e.Message);
+		}
+
+		throw new System.Exception("Got file descriptor");
+	}
+
+	public int GetFileSize()
+	{
+		return 0;
+	}
+
+	public byte[] ReadBytes(long Position, long Size)
+	{
+		return null;
+	}
+}
+
+
 
 public class FileReader : FileReaderBase
 {
@@ -49,6 +134,8 @@ public class FileReader : FileReaderBase
 	long FileSize;			//	the file view capcacity is bigger than the file size (page aligned) so we need to know the proper size
 #elif USE_FILE_HANDLE
 	System.IO.FileStream File;
+#elif USE_JAVA_FILEHANDLE
+	JavaFileReader File;
 #else
 	byte[] FileBytes;
 #endif
@@ -79,6 +166,8 @@ public class FileReader : FileReaderBase
 		return FileSize;
 #elif USE_FILE_HANDLE
 		return File.Length;
+#elif USE_JAVA_FILEHANDLE
+		return File != null ? File.GetFileSize() : 0;
 #else
 		return FileBytes != null ? FileBytes.Length : 0;
 #endif
@@ -97,15 +186,26 @@ public class FileReader : FileReaderBase
 		{
 			var AssetFilename = "Assets/" + Filename;
 			if (System.IO.File.Exists(AssetFilename))
+			{
+				Debug.Log("Assets/ filename exists:" + AssetFilename);
 				Filename = AssetFilename;
+			}
 
 			var StreamingAssetsFilename = Application.streamingAssetsPath + "/" + Filename;
 			if (System.IO.File.Exists(StreamingAssetsFilename))
+			{
+				Debug.Log("Streaming assets filename exists:" + StreamingAssetsFilename);
 				Filename = StreamingAssetsFilename;
+			}
+
 		}
 
+#if USE_JAVA_FILEHANDLE
+		//	dont check file exists on android
+#else
 		if (!System.IO.File.Exists(Filename))
 			throw new System.Exception("File missing: " + Filename);
+#endif
 
 		Debug.Log("FileReader opening file " + Filename + " (length = " + Filename.Length + ")");
 
@@ -119,6 +219,8 @@ public class FileReader : FileReaderBase
 		Debug.Log("Memory mapped FileSize = " + FileSize);
 #elif USE_FILE_HANDLE
 		File = System.IO.File.OpenRead(Filename);
+#elif USE_JAVA_FILEHANDLE
+		File = new JavaFileReader(Filename);
 #else
 		FileBytes = System.IO.File.ReadAllBytes(Filename);
 #endif
@@ -145,6 +247,8 @@ public class FileReader : FileReaderBase
 		if (BytesRead != Size)
 			throw new System.Exception("FileStream only read " + BytesRead + "/" + Size + " bytes");
 		return Data;
+#elif USE_JAVA_FILEHANDLE
+		return File.ReadBytes(Position, Size);
 #else
 		return FileBytes.SubArray(Position, Size);
 #endif
