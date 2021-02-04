353cafecc112ba2d42551bcad980063fb43297bb
Graham Reeves <graham@grahamreeves.com>
2021-02-04 16:54:30 +0000

    v1.3.22 Version bump for github action

--- Source/PopH264.cpp
+++ Source/PopH264.cpp
@@ -33,7 +33,8 @@ namespace PopH264
 	//	1.3.19	Android NDK MediaCodec implementation
 	//	1.3.20	Added PopH264_PushEndOfStream API as a clear wrapper for PopH264_PushData(null)
 	//	1.3.21	Added AllowBuffering option to decoder so by default LowLatency mode is on for Mediafoundation, which reduces buffering
-	const Soy::TVersion	Version(1,3,21);
+	//	1.3.22	Version bump for github build
+	const Soy::TVersion	Version(1,3,22);
 }
 
 
