c6eaa67db0b33d9bea8b99da0b18e3b68917e471
Graham Reeves <graham@grahamreeves.com>
2022-07-07 13:11:40 +0100

    Set correct (auto) product name for universal

--- Source_Ios/BuildUniversal.sh
+++ Source_Ios/BuildUniversal.sh
@@ -1,4 +1,6 @@
-# Type a script or drag a script file from your workspace to insert its path.
+# exit when any command fails
+set -e
+
 PROJECT_NAME=$1
 BUILDPATH_IOS="./build/${PROJECT_NAME}_Ios"
 BUILDPATH_SIM="./build/${PROJECT_NAME}_IosSimulator"
@@ -6,4 +8,4 @@ BUILDPATH_OSX="./build/${PROJECT_NAME}_Osx"
 xcodebuild archive -scheme ${PROJECT_NAME}_Ios -archivePath $BUILDPATH_IOS SKIP_INSTALL=NO -sdk iphoneos
 xcodebuild archive -scheme ${PROJECT_NAME}_Ios -archivePath $BUILDPATH_SIM SKIP_INSTALL=NO -sdk iphonesimulator
 xcodebuild archive -scheme ${PROJECT_NAME}_Osx -archivePath $BUILDPATH_OSX SKIP_INSTALL=NO
-xcodebuild -create-xcframework -framework ${BUILDPATH_IOS}.xcarchive/Products/Library/Frameworks/${PROJECT_NAME}_Ios.framework -framework ${BUILDPATH_SIM}.xcarchive/Products/Library/Frameworks/${PROJECT_NAME}_Ios.framework -framework ${BUILDPATH_OSX}.xcarchive/Products/Library/Frameworks/${PROJECT_NAME}_Osx.framework -output ./build/${FULL_PRODUCT_NAME}
+xcodebuild -create-xcframework -framework ${BUILDPATH_IOS}.xcarchive/Products/Library/Frameworks/${PROJECT_NAME}_Ios.framework -framework ${BUILDPATH_SIM}.xcarchive/Products/Library/Frameworks/${PROJECT_NAME}_Ios.framework -framework ${BUILDPATH_OSX}.xcarchive/Products/Library/Frameworks/${PROJECT_NAME}_Osx.framework -output ${BUILT_PRODUCTS_DIR}/${FULL_PRODUCT_NAME}
