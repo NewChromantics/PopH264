b9bf7026258a58f37b2293cd3b91df6a4845f90b
Graham Reeves <graham@grahamreeves.com>
2023-08-29 21:13:21 +0100

    v1.7.2 - DLL Plugin name changes again as mac editor was loading wrong version.

--- Source_CSharp/PopH264.cs
+++ Source_CSharp/PopH264.cs
@@ -11,10 +11,12 @@ using System.Collections.Generic;
 /// </summary>
 public static class PopH264
 {
-#if UNITY_WSA
-	private const string PluginName = "PopH264.Uwp.dll";	//	PopH264.Uwp.dll
-#elif UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
 	private const string PluginName = "PopH264";	//	libPopH264.dylib
+#elif UNITY_EDITOR_WIN
+	private const string PluginName = "PopH264";	//	PopH264.dll
+#elif UNITY_WSA
+	private const string PluginName = "PopH264.Uwp.dll";	//	PopH264.Uwp.dll
 #elif UNITY_IPHONE
 	private const string PluginName = "__Internal";
 #else
